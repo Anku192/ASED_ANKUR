ae67d33e3ac3f8b44a97526496e8973f9b407ea2
Anku192 <ankurprashantbalsebtech24@rvu.edu.in>
2026-02-27 14:51:11 +0530

    Update ased_lab_full_stack.py

--- ased_lab_full_stack.py
+++ ased_lab_full_stack.py
@@ -20,7 +20,7 @@ import sklearn
 from sklearn import model_selection
 from sklearn import linear_model
 
-X=[[4.0],[5.0],[6.0],[7.0],[8.0],[9.0],[10.0]]
+X=[[4.0],[5.0],[6.0],[7.0],[8.0],[9.0],[10.0],[20.0]]
 Y=[8,10,12,14,16,18,20]
 X_train,X_test,Y_train,Y_test=model_selection.train_test_split(X,Y,test_size=0.3,random_state=7)
 print("Trining Features",X_train);print("Training Labels",Y_train)
